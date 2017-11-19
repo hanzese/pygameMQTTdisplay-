f6b39ef739cfe6c222330de9c0c10d7370d48f1d
Hannes <hanzese@gmail.com>
2017-11-19 13:34:03 +0200

    added line for windows python mixser init

--- Main.py
+++ Main.py
@@ -12,6 +12,7 @@ datavr = "No data"
 pygame.init()
 screen = pygame.display.set_mode((1024, 200))
 clock = pygame.time.Clock()
+pygame.mixer.init(44100, -16,2,2048)
 pygame.mixer.music.load("thrown.mp3")
 
 done = False
