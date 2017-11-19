2cdebe7c62be84b818e21feb07ccc3016ae81f8e
Hannes <hanzese@gmail.com>
2017-11-19 12:06:05 +0200

    Adding notyfiing sound

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 import subprocess
 
+
 proc = subprocess.Popen('python Mqtt_OK.py',
                         shell=True,
                         stdin=subprocess.PIPE,
@@ -11,10 +12,12 @@ datavr = "No data"
 pygame.init()
 screen = pygame.display.set_mode((320, 200))
 clock = pygame.time.Clock()
+pygame.mixer.music.load("thrown.mp3")
+
 done = False
 
 font = pygame.font.SysFont("comicsansms", 40)
-text = font.render(datavr, True, (0, 128, 0))
+text = font.render(datavr.encode('utf8'), True, (0, 128, 0))
 
 while not done:
     output = proc.stdout.readline()
@@ -24,18 +27,20 @@ while not done:
     else:
         print "data tuleb"
         print datavr
+        if datavr == "Alused otsas!!":
+            pygame.mixer.music.play()
         text = font.render(datavr, True, (0, 128, 0))
         datavr = ""
         for event in pygame.event.get():
-            print event
             if event.type == pygame.QUIT:
+                sys.exit(0)
                 done = True
                 break
 
 
-    screen.fill((255, 255, 255))
+    screen.fill((255,255,255))
     screen.blit(text,(160 - text.get_width() // 2, 100 - text.get_height() // 2))
     pygame.display.flip()
-    #change_varible()
+    # change_varible()
 
     clock.tick(60)
